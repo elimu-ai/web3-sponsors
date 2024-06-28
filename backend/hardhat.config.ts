ecd858637b6faef4597d756f80e9469caf204fac
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-06-28 16:56:13 +0700

    chore(backend): deploy to base sepolia
    refs #16

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,9 +1,47 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24"
+  },
   gasReporter: {
+    enabled: true
+  },
+  networks: {
+    base_sepolia: { // Chain ID 84532
+      url: "https://sepolia.base.org",
+      accounts: [process.env.WALLET_KEY as string]
+    },
+    base_mainnet: { // Chain ID 8453
+      url: "https://mainnet.base.org",
+      accounts: [process.env.WALLET_KEY as string]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: "base_sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org"
+        }
+      },
+      {
+        network: "base_mainnet",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.basescan.org/api",
+          browserURL: "https://basescan.org"
+        }
+      }
+    ]
+  },
+  sourcify: {
     enabled: true
   }
 };
