de3084dbccd1e0ec6fba638ad9bfb8b75e2e026f
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-09-09 20:03:38 +0700

    refactor: remove language code from sponsorships

--- backend/ignition/modules/SponsorshipQueue.ts
+++ backend/ignition/modules/SponsorshipQueue.ts
@@ -6,17 +6,7 @@ const SponsorshipQueueModule = buildModule("SponsorshipQueueModule", (m) => {
 
   const estimatedCost = ethers.parseUnits("0.0001");
 
-  let languagesAddress = ethers.ZeroAddress;
-  if (network.name == "sepolia") {
-    // https://github.com/elimu-ai/web3-smart-contracts/blob/main/dao-contracts/ignition/deployments/chain-11155111/deployed_addresses.json
-    languagesAddress = "0x9A3033D2e237376a09d7e19A7479622F8Ae38557";
-  } else if (network.name == "mainnet") {
-    // https://github.com/elimu-ai/web3-smart-contracts/blob/main/dao-contracts/ignition/deployments/chain-1/deployed_addresses.json
-    languagesAddress = "0x addr mainnet";
-  }
-  console.log("languagesAddress:", languagesAddress);
-
-  const sponsorshipQueue = m.contract("SponsorshipQueue", [estimatedCost, languagesAddress]);
+  const sponsorshipQueue = m.contract("SponsorshipQueue", [estimatedCost]);
 
   return { sponsorshipQueue };
 });
