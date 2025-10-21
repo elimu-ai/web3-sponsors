bf6917378e1638985682e90cd828e7d2753c51db
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-10-21 17:30:52 +0700

    feat(frontend): load estimated cost
    #76

--- backend/ignition/modules/SponsorshipQueue.ts
+++ backend/ignition/modules/SponsorshipQueue.ts
@@ -4,7 +4,7 @@ import { ethers, network } from "hardhat";
 const SponsorshipQueueModule = buildModule("SponsorshipQueueModule", (m) => {
   console.log("network.name:", network.name);
 
-  const estimatedCost = ethers.parseUnits("0.0001");
+  const estimatedCost = ethers.parseUnits("0.0002");
 
   const sponsorshipQueue = m.contract("SponsorshipQueue", [estimatedCost]);
 
