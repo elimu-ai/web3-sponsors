4f4df8394306e70fb55ca5935b362be2942d601d
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-10-24 15:16:38 +0700

    refactor(frontend): use mapped elemented instead of indexing
    #116

--- frontend/src/components/ProcessedQueuePairs.tsx
+++ frontend/src/components/ProcessedQueuePairs.tsx
@@ -51,7 +51,7 @@ function LoadQueuePairProcessedEvents() {
         events.map((el, i) =>
             <div key={i} className="mt-4 p-4 text-2xl bg-zinc-50 dark:bg-zinc-900 rounded-lg">
                 <div className="text-zinc-400">
-                    Block: #{Number(events[i].blockNumber)} ({new Date(Number(events[i].blockTimestamp) * 1_000).toISOString().substring(0,10)} {new Date(Number(events[i].blockTimestamp) * 1_000).toISOString().substring(11,16)})
+                    Block: #{Number(el.blockNumber)} ({new Date(Number(el.blockTimestamp) * 1_000).toISOString().substring(0,10)} {new Date(Number(el.blockTimestamp) * 1_000).toISOString().substring(11,16)})
                 </div>
                 <div className="mt-4 flex gap-x-4">
                     <div className="skew-y-3 p-4 text-2xl bg-purple-200 dark:bg-purple-950 rounded-lg border-purple-400 border-r-4 border-b-4 hover:border-r-8 hover:border-b-8 hover:-translate-y-1">
@@ -59,11 +59,11 @@ function LoadQueuePairProcessedEvents() {
                             💜
                         </div>
                         <div className="mt-2">
-                            {new Date(Number(events[i].args[1].timestamp) * 1_000).toISOString().substring(0,10)} {new Date(Number(events[i].args[1].timestamp) * 1_000).toISOString().substring(11,16)}
+                            {new Date(Number(el.args[1].timestamp) * 1_000).toISOString().substring(0,10)} {new Date(Number(el.args[1].timestamp) * 1_000).toISOString().substring(11,16)}
                         </div>
-                        Amount: {formatEther(events[i].args[1].estimatedCost)} ETH
+                        Amount: {formatEther(el.args[1].estimatedCost)} ETH
                         <div className="mt-2">
-                            Sponsor: <code>{events[i].args[1].sponsor.substring(0, 6)}...{events[i].args[1].sponsor.substring(38, 42)}</code>
+                            Sponsor: <code>{el.args[1].sponsor.substring(0, 6)}...{el.args[1].sponsor.substring(38, 42)}</code>
                         </div>
                     </div>
                     <div className="skew-y-3 p-4 text-2xl bg-indigo-200 dark:bg-indigo-950 rounded-lg border-indigo-400 border-r-4 border-b-4 hover:border-r-8 hover:border-b-8 hover:-translate-y-1">
@@ -71,16 +71,16 @@ function LoadQueuePairProcessedEvents() {
                             🛵💨
                         </div>
                         <div className="mt-2">
-                            {new Date(Number(events[i].args[0].timestamp) * 1_000).toISOString().substring(0,10)} {new Date(Number(events[i].args[0].timestamp) * 1_000).toISOString().substring(11,16)}
+                            {new Date(Number(el.args[0].timestamp) * 1_000).toISOString().substring(0,10)} {new Date(Number(el.args[0].timestamp) * 1_000).toISOString().substring(11,16)}
                         </div>
                         <div className="mt-2">
-                            Language: {events[i].args[0].languageCode}
+                            Language: {el.args[0].languageCode}
                         </div>
                         <div className="mt-2">
-                            Android ID: {events[i].args[0].androidId}
+                            Android ID: {el.args[0].androidId}
                         </div>
                         <div className="mt-2">
-                            Distributor: <code>{events[i].args[0].distributor.substring(0, 6)}...{events[i].args[0].distributor.substring(38, 42)}</code>
+                            Distributor: <code>{el.args[0].distributor.substring(0, 6)}...{el.args[0].distributor.substring(38, 42)}</code>
                         </div>
                     </div>
                 </div>
