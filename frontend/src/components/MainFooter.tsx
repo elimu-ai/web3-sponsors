af671cf6b44e2aa8e09102d43f48f196f7056740
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-12-06 10:31:39 +0700

    feat(frontend): add link to github release

--- frontend/src/components/MainFooter.tsx
+++ frontend/src/components/MainFooter.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import package_json from "../../package.json"
 
 export default function MainFooter() {
@@ -9,7 +10,7 @@ export default function MainFooter() {
     return (
         <footer className="mt-16 p-8 flex justify-center w-full border-purple-100 dark:border-purple-950 border-t-2">
             Made with <span className="animate-pulse">💜</span> by&nbsp;<a href="https://elimu.ai" className="text-purple-600">elimu.ai</a>
-            <code className="ml-8 px-2 pt-0.5 text-sm text-gray-400 bg-zinc-800 rounded-lg">v{packageVersion}</code>
+            <Link className="ml-8" href="https://github.com/elimu-ai/web3-sponsors/releases" target="_blank"><code className="px-2 py-1 text-sm text-gray-400 bg-zinc-800 rounded-lg">v{packageVersion}</code></Link>
         </footer>
     )
 }
