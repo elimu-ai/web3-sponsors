fab63545dd070d5e49713ab0460635c7f78af9b0
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-12-18 14:01:48 +0100

    release 0.9.4

--- backend/contracts/SponsorshipQueue.sol
+++ backend/contracts/SponsorshipQueue.sol
@@ -9,7 +9,10 @@ struct Sponsorship {
     address sponsor;
 }
 
+/// @title A queue of sponsorships
+/// @author Ξlimu DAO
 /// @notice A queue of sponsorships for the Ξlimu DAO's education sponsorship program (https://sponsors.elimu.ai)
+/// @dev The address of the queue handler smart contract needs to be set after deployment
 contract SponsorshipQueue is ProtocolVersion {
     address public owner;
     uint256 public estimatedCost;
@@ -30,24 +33,32 @@ contract SponsorshipQueue is ProtocolVersion {
         estimatedCost = estimatedCost_;
     }
 
+    /// @notice Change the owner of this smart contract
+    /// @param owner_ The address of the new owner
     function updateOwner(address owner_) public {
         require(msg.sender == owner, "Only the current owner can set a new owner");
         owner = owner_;
         emit OwnerUpdated(owner_);
     }
 
+    /// @notice Update the amount that a sponsor will have to pay
+    /// @param estimatedCost_ The new estimated cost
     function updateEstimatedCost(uint256 estimatedCost_) public {
         require(msg.sender == owner, "Only the owner can set the `estimatedCost`");
         estimatedCost = estimatedCost_;
         emit EstimatedCostUpdated(estimatedCost_);
     }
 
+    /// @notice Change the handler of this queue
+    /// @param queueHandler_ The address of the new queue handler smart contract
     function updateQueueHandler(address queueHandler_) public {
         require(msg.sender == owner, "Only the owner can set the `queueHandler` address");
         queueHandler = queueHandler_;
         emit QueueHandlerUpdated(queueHandler_);
     }
 
+    /// @notice Pay ETH to this smart contract
+    /// @dev The amount of ETH to pay can be changed by adjusting the `estimatedCost` variable
     function addSponsorship() public payable {
         require(msg.value == estimatedCost, "Must send exactly the estimated cost");
         Sponsorship memory sponsorship = Sponsorship(
@@ -59,11 +70,16 @@ contract SponsorshipQueue is ProtocolVersion {
         emit SponsorshipAdded(queueNumberNext - 1, msg.sender);
     }
 
+    /// @dev Internal function to add a sponsorship to the queue
+    /// @param sponsorship The sponsorship to add
     function enqueue(Sponsorship memory sponsorship) private {
         queue[queueNumberNext] = sponsorship;
         queueNumberNext += 1;
     }
 
+    /// @notice Remove and return the next sponsorship from the queue
+    /// @dev Only callable by the queue handler
+    /// @return The sponsorship that was removed from the front of the queue
     function dequeue() public returns (Sponsorship memory) {
         require(msg.sender == queueHandler, "Only the queue handler can remove from the queue");
         require(getLength() > 0, "Queue is empty");
@@ -72,10 +88,16 @@ contract SponsorshipQueue is ProtocolVersion {
         return sponsorship;
     }
 
+    /// @notice Get the current length of the queue
+    /// @return The number of sponsorships in the queue (that have not yet been processed)
     function getLength() public view returns (uint256) {
         return queueNumberNext - queueNumberFront;
     }
 
+    /// @notice Transfer ETH from a sponsorship to a distributor
+    /// @dev Only callable by the queue handler
+    /// @param distributor The address to receive the ETH
+    /// @param sponsorship The sponsorship containing the amount to transfer
     function payDistributor(address distributor, Sponsorship memory sponsorship) public {
         require(msg.sender == queueHandler, "Only the queue handler can process payouts");
         payable(distributor).transfer(sponsorship.estimatedCost);
