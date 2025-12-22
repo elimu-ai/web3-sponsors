eae3f12513033028c724d2ed236cc52235540d3c
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-12-22 19:19:23 +0100

    Update CommunityFund.sol

--- backend/contracts/sample/CommunityFund.sol
+++ backend/contracts/sample/CommunityFund.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import { ISponsorshipQueue } from "@elimu-ai/web3-sponsors/ISponsorshipQueue.sol";
+import { ISponsorshipQueue } from "@elimu-ai/sponsors/ISponsorshipQueue.sol";
 
 contract CommunityFund {
     ISponsorshipQueue public immutable sponsorshipQueue;
