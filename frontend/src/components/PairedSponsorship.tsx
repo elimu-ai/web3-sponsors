dcce1e43f4f5c791e3c5d9be5f0849b1fe3bd04b
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-12-26 18:38:33 +0100

    feat(frontend): add info icon
    #143

--- frontend/src/components/PairedSponsorship.tsx
+++ frontend/src/components/PairedSponsorship.tsx
@@ -50,7 +50,9 @@ export default function PairedSponsorship({ distributionQueueNumber }: { distrib
     if (!sponsorshipQueueNumber) {
         return (
             <span className="p-2 text-gray-300 border-gray-400 bg-gray-700 border-2 rounded-xl">
-                Pairing not yet <Link href="/process" className="text-purple-300" id="processLink">processed</Link>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 inline mb-1 mr-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                </svg> Pairing not yet <Link href="/process" className="text-purple-300" id="processLink">processed</Link>
             </span>
         )
     } else {
